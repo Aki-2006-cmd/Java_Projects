4b2863145db5ad843f95203aed9c4086ce2eaa2e
Akash Gamage <gamage.gdpa@gmail.com>
2026-08-18 20:24:13 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+
+    public static void main(String[] args){
+
+        // orinting the output
+        System.out.print("Akash!");
+
+        // println or at last \n for the next line
+
+        /*
+            this is a multi line comment
+            gtwgs
+            sgs
+         */
+    }
+}
