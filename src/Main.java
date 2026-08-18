8dc6d4c3ef6ba0bedbdd335208cd5fc604609388
Akash Gamage <gamage.gdpa@gmail.com>
2026-08-18 23:29:01 +0530

    added student class logic

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
         // orinting the output
         System.out.print("Akash!");
-
+        // test github line
         // println or at last \n for the next line
-
+        System.out.print("THE LORD IS ME");
         /*
             this is a multi line comment
             gtwgs
