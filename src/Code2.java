9c83d02a2a4bbace45c8e28225a9286ea9d4e66a
Akash Gamage <gamage.gdpa@gmail.com>
2026-08-22 21:54:27 +0530

    added student class logic

--- src/Code2.java
+++ src/Code2.java
@@ -1,13 +1,46 @@
+
+// Variables
+
+
 public class Code2 {
     public static void main(String [] args){
 
 
         int x = 43;
         int y = 43245;
-
         int sum = x + y;
-
         System.out.println(sum);
 
-        }
+        byte by = 3;
+        short bye = 3333;
+        System.out.println(by);
+        System.out.println(bye);
+
+        float f = 5.34636f;
+        System.out.println(f);
+
+        char c = 'f';// cannot add a string line
+        System.out.println(c);
+
+        String message = "hello world";
+        String word = "Apple";
+        System.out.println(message);System.out.println(word); // orientation is not a matter
+
+
+        //literals
+        int num1 = 0b101;
+        System.out.println(num1);
+
+        int num2 = 10_00_00_00_00; // java wala mehem puluwan. under score ek ewa wen krnn
+        System.out.println(num2);
+
+        double num3 = 542;
+        System.out.println(num3);
+
+        num3++;
+        System.out.println(num3);
+
+
+
+    }
 }
---
+++ src/Code3.java
@@ -0,0 +1,27 @@
+// Type Casting and Conversion
+
+public class Code3 {
+
+    public static void Main(String[] a){
+
+        byte b = 127;
+        int c = 12;
+
+        // b = c cannot be done. so need to be type casted
+
+        b = (byte) c ; // type casting
+                       // explicit conversion
+
+        // implicit conversion
+        // c = b;
+
+
+
+
+
+
+
+    }
+
+
+}
