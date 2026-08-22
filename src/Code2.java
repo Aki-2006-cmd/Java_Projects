8beffd963194d38600242206f6d68f0eb7afb492
Akash Gamage <gamage.gdpa@gmail.com>
2026-08-22 15:17:39 +0530

    added student class logic

---
+++ src/Code2.java
@@ -0,0 +1,13 @@
+public class Code2 {
+    public static void main(String [] args){
+
+
+        int x = 43;
+        int y = 43245;
+
+        int sum = x + y;
+
+        System.out.println(sum);
+
+        }
+}
