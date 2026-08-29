704c65579cea0a5da20c2dade1409303c5e9450a
Akash Gamage <gamage.gdpa@gmail.com>
2026-08-29 22:09:16 +0530

    added student class logic

---
+++ src/Code10.java
@@ -0,0 +1,25 @@
+public class Code10 {
+    public static void main ( String[] a){
+        while (true) {
+            System.out.println("hi");
+            break;
+        }
+
+        int x = 5;
+        while (x > 1){
+            x++;
+            System.out.println(x);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/Code9.java
+++ src/Code9.java
@@ -1,6 +1,31 @@
 public class Code9 {
     public static void main (String[] a){
+         int n = 1;
 
+         switch (n) {
+             case 1:
+                 System.out.println("MOnday");
+                 break;
+             case 2:
+                 System.out.println("Tuesday");
+                 break;
+         }
+
+         // latest switch use
+
+        String day = "Mnday";
+
+         switch (day){
+             case "Monday" , "Sunday":
+                 System.out.println("sleep");
+                 break;
+             case "Tuesday":
+                 System.out.println("wakeup");
+                 break;
+             default:
+                 System.out.println("goon");
+                 break;
+         }
 
 
 
