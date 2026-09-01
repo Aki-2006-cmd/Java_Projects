1d1f8c73790549f28caa81ca50e09e913bd919d6
Akash Gamage <gamage.gdpa@gmail.com>
2026-09-01 19:06:12 +0530

    added student class logic

--- src/Code12.java
+++ src/Code12.java
@@ -3,9 +3,25 @@
 
 public class Code12 {
     public static void main (String[] a){
+        // python wala wage methn range kiyl function ekka na.
+        // ethkot mekt galapenna kramayaik thiyenawa
 
 
+        for     ( int x = 0   ; x <10     ; x++ ) {
+            /* initialization ; condition ; step */
 
+            // body
+            System.out.println("Hello " + x);
+
+            for (int y = 0 ; y < 7 ; y++ ){
+                System.out.print(" DAY_" + y);
+
+                if ( y == 6 ){
+                    System.out.println();
+
+                }
+            }
+        }
 
 
 
