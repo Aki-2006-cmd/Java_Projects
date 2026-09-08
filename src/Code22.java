b5b1b2ec34b8e0edd99ab772e9def3f04b46238d
Akash Gamage <gamage.gdpa@gmail.com>
2026-09-09 00:20:38 +0530

    added student class logic

--- src/Code22.java
+++ src/Code22.java
@@ -1,2 +1,31 @@
+// String buffer and StringBuilder
+
 public class Code22 {
+    public static void main ( String[] a){
+
+        StringBuffer sb = new StringBuffer("Akash");
+        System.out.println(sb.capacity());
+        System.out.println(sb.length());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
---
+++ src/Practise5.java
@@ -0,0 +1,12 @@
+//
+
+public class Practise5 {
+    public static void main ( String[] a){
+
+
+
+
+
+
+    }
+}
